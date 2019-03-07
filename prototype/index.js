a4dad2a6957f4c80e548f7f75589d9d3acd3ac8b
bhambleton <35317075+bhambleton@users.noreply.github.com>
2019-03-06 23:12:36 -0800

    Add files via upload
    Add prototype folder for simple viewing/editing of how the app might look

---
+++ prototype/index.js
@@ -0,0 +1,14 @@
+function shelterSearch() {
+
+}
+
+function tempWorkSearch() {
+  
+}
+
+
+var tempWorkButton = document.getElementById('tempWorkButton');
+tempWorkButton.addEventListener('click', tempWorkSearch);
+
+var shelterButton = document.getElementById('shelterButton');
+shelterButton.addEventListener('click', shelterSearch);
