94b5a61871b8ef98266066a9e146d119330aa4da
Ofrty <24553692+Ofrty@users.noreply.github.com>
2019-03-07 20:55:57 -0500

    integrated prototype; client home buttons assigned event listener
    - integrated Brian's prototype stuff into the handlebars templates & style
    - generalized event listener to supply the id of the button, which the server can use to know what button was pressed

---
+++ app/server/public/scripts/client.js
@@ -0,0 +1,46 @@
+/**********************************************************************************************************************
+ * Author: CS361 Group 14
+ * Date Created: 2019/03/06
+ * Description: Client-side scripts.
+ *
+ *********************************************************************************************************************/
+
+//find buttons
+var buttons = document.getElementsByTagName('button');
+
+//debug
+//console.log('client.js is adding event listeners to buttons, of which there are ' + buttons.length);
+
+//loop through event listener and set on all buttons, passing the button's id so the server knows which button was clicked
+for (var i = 0; i < buttons.length; i++)
+{
+    //set cur button
+    var b = buttons[i];
+
+    //debug
+    //console.log("adding event listener to button " + b + " which has id " + b.id);
+
+    b.addEventListener('click', function(id)
+    {
+        //can i get a little closure
+        return function()
+        {
+            //get zip or set zip to null if empty
+            var zip = document.getElementsByName('zipcodeInput')[0].value;
+            if (!zip)
+            {
+                //debug
+                //console.log('zip was not supplied, setting zip = NULL');
+
+                zip = null;
+            }
+
+            //report to browser console
+            console.log('Client about to send request for resource ' + id + ' in zip ' + zip);
+
+            //send info to server
+            var req = new XMLHttpRequest();
+            //yadayada
+        }
+    }(b.id));
+}
